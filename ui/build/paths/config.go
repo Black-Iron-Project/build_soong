d7ea872aa23c49f2bcd21b8d389a7a1a368b9cc7
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-03-01 13:18:50 +0800

    config.go: Allow running numfmt
    Change-Id: I81c2e37e966f0f5d407cf7bd2b9ca3fb3f025c60
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -107,6 +107,7 @@ var Configuration = map[string]PathConfig{
 	"llvm-addr2line": Allowed,
 	"locale":      Allowed,
 	"lsof":        Allowed,
+	"numfmt":      Allowed,
 	"openssl":     Allowed,
 	"patch":       Allowed,
 	"pkg-config":  Allowed,
